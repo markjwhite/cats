73dd6721c889ff5cdac945cf4ebf45086b3d9043
markjwhite <markwhite259@gmail.com>
2021-05-04 20:38:13 -0400

    added mocha chai unit testing

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,12 +1,17 @@
 // asyncBreeds.js
 const fs = require('fs');
+const { showCompletionScript } = require('yargs');
 
 const breedDetailsFromFile = function(breed, callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) callback(data);
+    if (!error) {
+      callback(data);
+    } else {
+      callback(undefined);
+    }
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
@@ -20,3 +25,5 @@ const breedDetailsFromFile = function(breed, callback) {
 breedDetailsFromFile('Bombay', data => {
   console.log('Return Value: ', data)
 }); // => will NOT print out details, instead we will see undefined!
+
+module.exports = breedDetailsFromFile;
